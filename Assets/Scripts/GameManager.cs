b9c16166c43e4254ebb71836841fc033cf342c4e
Martin Engelsgjerd <martin.engelsgjerd@lyse.net>
2018-03-15 20:20:08 -0700

    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	
+
+	public static GameManager instance = null;
+	public GameManager gameManager;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+		
+	}
+
+
+
+}
