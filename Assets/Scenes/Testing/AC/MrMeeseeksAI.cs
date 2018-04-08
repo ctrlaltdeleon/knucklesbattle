bf1ce6c23a89bce69d4ddc28d5e422b5770dcff9
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-04-08 13:34:22 -0700

    [+] AI surrounds player wherever they go

---
+++ Assets/Scenes/Testing/AC/MrMeeseeksAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrMeeseeksAI : MonoBehaviour {
+    float timeCounter = 0;
+    float speed;
+    float radius;
+	public Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        speed = 20;
+        radius = 2;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timeCounter += Time.deltaTime * speed;
+		pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+		float x = pos.x + (Mathf.Cos(timeCounter) * radius);
+        float y = 0;
+		float z = pos.z + (Mathf.Sin(timeCounter) * radius);
+
+        transform.position = new Vector3(x, y, z); 
+	}
+}
