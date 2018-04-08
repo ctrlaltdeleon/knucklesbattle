f6b8c88de7dfd052338badfea47009b943b42312
bryevo <ovnairb@gmail.com>
2018-04-08 16:38:40 -0700

    Knuckles Health bar that rotates to user perspective

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,26 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public GameObject Knuckles;
+    private List<Vector3> randomSpawnPositions = new List<Vector3>();
+    public float spawnRate = 1.0f;
+    private float nextSpawn;
 
-	
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
 
-	public static GameManager instance = null;
-	public GameManager gameManager;
+        nextSpawn = 0.0f;
+        for (int i = 0; i < 30; i++)
+        {
+            float randomPos = Random.Range(-250, 250);
+            randomSpawnPositions.Add(new Vector3(-250, 0, randomPos));
+            randomSpawnPositions.Add(new Vector3(250, 0, randomPos));
+            randomSpawnPositions.Add(new Vector3(randomPos, 0, -250));
+            randomSpawnPositions.Add(new Vector3(randomPos, 0, 250));
+        }
+    }
 
 
-
-	// Use this for initialization
-	void Awake () {
-		if (instance == null) {
-			instance = this;
-		} else if (instance != this) {
-			Destroy (gameObject);
-		}
-		DontDestroyOnLoad (gameObject);
-		
-	}
-
-
-
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            Debug.Log("creating Knuckles");
+            nextSpawn = Time.time + spawnRate;
+            int index = Random.Range(0, randomSpawnPositions.Count);
+            Knuckles.transform.root.position = randomSpawnPositions[index];
+            Instantiate(Knuckles);
+        }
+    }
 }
--- Assets/Scripts/KnucklesController.cs
+++ Assets/Scripts/KnucklesController.cs
@@ -5,16 +5,66 @@ using UnityEngine;
 public class KnucklesController : MonoBehaviour
 {
     public List<Texture> colors;
+    public float speed;
+    public GameObject tower;
+    public int type;
+    public float hp;
 
     void Awake()
     {
+        // Change the color of knuckles
         int randomColor = Random.Range(0, colors.Count);
         gameObject.transform.GetChild(1).GetComponent<Renderer>().materials[1].mainTexture =
             colors[randomColor];
+        type = randomColor;
+
+        // Set attributes based on color
+        switch (type)
+        {
+            case (int) Knuckles.Red:
+                speed = 10;
+                hp = 20;
+                break;
+            case (int) Knuckles.Blue:
+                gameObject.transform.localScale *= 2.5f;
+                transform.GetChild(2).localScale /= 2.5f;
+                speed = 7.5f;
+                hp = 40;
+                break;
+            case (int) Knuckles.Green:
+                speed = 10;
+                hp = 15;
+                break;
+            case (int) Knuckles.Orange:
+                speed = 30;
+                hp = 15;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveTowards(tower.transform.position);
     }
+
+
+    //Moves towards target 
+    void MoveTowards(Vector3 target)
+    {
+        gameObject.transform.rotation = Quaternion.Euler(0f, gameObject.transform.rotation.y, 0f);
+        // Looks at the target
+        gameObject.transform.LookAt(new Vector3(target.x, 0, target.z));
+        gameObject.transform.position =
+            Vector3.MoveTowards(gameObject.transform.position, new Vector3(target.x, 0, target.z),
+                speed * Time.deltaTime);
+    }
+}
+
+enum Knuckles
+{
+    Red = 0,
+    Blue = 1,
+    Green = 2,
+    Orange = 3
 }
