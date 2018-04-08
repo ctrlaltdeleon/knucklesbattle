566426c67d951ad8a7e71b7a13df5504ca1f8396
bryevo <ovnairb@gmail.com>
2018-04-08 13:48:59 -0700

    Randomly Color Generated Knuckles

---
+++ Assets/Scripts/KnucklesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnucklesController : MonoBehaviour
+{
+    public List<Texture> colors;
+
+    void Awake()
+    {
+        int randomColor = Random.Range(0, colors.Count);
+        gameObject.transform.GetChild(1).GetComponent<Renderer>().materials[1].mainTexture =
+            colors[randomColor];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
