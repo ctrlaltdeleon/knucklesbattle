d5ccfd7746923445f1c3d11b28a253ebf08199be
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-04-08 12:37:33 -0700

    [+] Upload to AC comp, oscillation AI done

---
+++ Assets/Scripts/MrMeeseeksAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrMeeseeksAI : MonoBehaviour {
+    float timeCounter = 0;
+    float speed;
+    float radius;
+
+	// Use this for initialization
+	void Start () {
+        speed = 2;
+        radius = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCounter += Time.deltaTime * speed;
+
+        float x = (Mathf.Cos(timeCounter) * radius);
+        float y = 0;
+        float z = (Mathf.Sin(timeCounter) * radius);
+
+        transform.position = new Vector3(x, y, z); 
+	}
+}
